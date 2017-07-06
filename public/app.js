1d621e75eb1274484cebdaae5f4d1be2a665f7af
mkhizeryounas <m.khizeryounas@gmail.com>
2017-07-06 16:46:20 +0500

    http intercepter

--- public/app.js
+++ public/app.js
@@ -4,7 +4,18 @@ var app = angular.module('cartify', [
   'angular-loading-bar',
 
 ]);
-
+app.config(function($httpProvider) {
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+  $httpProvider.interceptors.push('httpRequestInterceptor');
+});
+app.factory('httpRequestInterceptor', function ($localStorage, $rootScope) {
+ return {
+   request: function (config) {
+    
+    return config;
+   }
+ };
+});
 app.run(function($localStorage, $state, $rootScope, $http) {
 	$rootScope.$state = $state;
 	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams, options) {
